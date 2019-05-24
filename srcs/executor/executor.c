1dbdfd5fa72722ce0f88154c84e3ea1ef9bca9fa
skenzou <47005532+skenzou@users.noreply.github.com>
2019-05-24 16:20:16 +0200

    peaufinement

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 16:15:41 by midrissi          #+#    #+#             */
-/*   Updated: 2019/05/24 14:15:14 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/05/24 16:11:04 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ static void		ft_execute(char **args, int redir)
 {
 	if (!ft_pre_execution(&args, redir))
 		ft_fork(args, g_shell->env);
+	if (redir)
+		restore_fd();
 }
 
 void	ft_execute_ast(t_ast *root, char **env)
@@ -32,10 +34,7 @@ void	ft_execute_ast(t_ast *root, char **env)
 	if (root->right)
 		ft_execute_ast(root->right, env);
 	if (root->token->redir)
-	{
 		ft_execute(root->token->content, 1);
-		restore_fd();
-	}
 	else if (root->token->type == TOKEN_WORD)
 		ft_execute(root->token->content, 0);
 }
