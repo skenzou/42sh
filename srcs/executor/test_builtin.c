50a29c9d4af11a6be7fd54781d681ca221d324c2
ghariani <gmelek@student.42.fr>
2019-06-16 16:58:59 +0200

    builtin test fix bug

--- srcs/executor/test_builtin.c
+++ srcs/executor/test_builtin.c
@@ -6,93 +6,92 @@
 /*   By: ghamelek <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 00:34:44 by ghamelek          #+#    #+#             */
-/*   Updated: 2019/06/12 15:39:15 by ghamelek         ###   ########.fr       */
+/*   Updated: 2019/06/16 16:58:08 by ghamelek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
-#include <sys/stat.h>
 
-static int is_integer(char *str,char *str2)
+static int	is_integer(char *str, char *str2)
 {
 	int i;
 
 	i = -1;
 	while (str[++i] && ft_isdigit(str[i]))
-		continue ;	
+		continue ;
 	if (str[i])
 	{
-		ft_putstr_fd("42sh: integer expression expected: ",2);
+		ft_putstr_fd("42sh: integer expression expected: ", 2);
 		ft_putendl_fd(str, 2);
-		return(2);
+		return (2);
 	}
 	i = -1;
 	while (str2[++i] && ft_isdigit(str2[i]))
-		continue ;	
+		continue ;
 	if (str2[i])
 	{
-		ft_putstr_fd("42sh: integer expression expected: ",2);
+		ft_putstr_fd("42sh: integer expression expected: ", 2);
 		ft_putendl_fd(str2, 2);
-		return(2);
+		return (2);
 	}
 	return (0);
 }
 
-static int arithmetic_test(char **av,char *op)
+static int	arithmetic_test(char **av, char *op)
 {
-	if(is_integer(av[1], av[3]) )
+	if (is_integer(av[1], av[3]))
 		return (2);
-	if (!ft_strcmp(op,"-eq")) 	
+	if (!ft_strcmp(op, "-eq"))
 		return (!(ft_atoi(av[1]) == ft_atoi(av[3])));
-	else if (!ft_strcmp(av[2],"-ne"))						 
-			return (ft_atoi(av[1]) == ft_atoi(av[3]));
-	else if (!ft_strcmp(av[2],"-ge")) 						 
-			return (ft_atoi(av[1]) < ft_atoi(av[3]));
-	else if (!ft_strcmp(av[2],"-lt"))						 
-			return (ft_atoi(av[1]) >= ft_atoi(av[3]));
+	else if (!ft_strcmp(av[2], "-ne"))
+		return (ft_atoi(av[1]) == ft_atoi(av[3]));
+	else if (!ft_strcmp(av[2], "-ge"))
+		return (ft_atoi(av[1]) < ft_atoi(av[3]));
+	else if (!ft_strcmp(av[2], "-lt"))
+		return (ft_atoi(av[1]) >= ft_atoi(av[3]));
 	return (ft_atoi(av[1]) > ft_atoi(av[3]));
 }
 
 static int	binary_test(char **av)
 {
-	if(!ft_strcmp(av[2],"=")) 								 
-		return ((ft_strcmp(av[1],av[3]) != 0));
-	else if (!ft_strcmp(av[2],"!=")) 								 
-		return ((ft_strcmp(av[1],av[3]) == 0));
-	else if (!ft_strcmp(av[2],"-eq")) 	
-		return (arithmetic_test(av,"-eq"));	
-	else if (!ft_strcmp(av[2],"-ne"))						 
-		return (arithmetic_test(av,"-ne"));	
-	else if (!ft_strcmp(av[2],"-ge")) 						 
-		return (arithmetic_test(av,"-ge"));	
-	else if (!ft_strcmp(av[2],"-lt"))						 	
-		return (arithmetic_test(av,"-lt"));	
-	else if (!ft_strcmp(av[2],"-le")) 						 
-		return (arithmetic_test(av,"-le"));	
-	else if(av[2][0] == '-')
-	{	
-		ft_putstr_fd("42sh: unknown condition: ",2);
+	if (!ft_strcmp(av[2], "="))
+		return ((ft_strcmp(av[1], av[3]) != 0));
+	else if (!ft_strcmp(av[2], "!="))
+		return ((ft_strcmp(av[1], av[3]) == 0));
+	else if (!ft_strcmp(av[2], "-eq"))
+		return (arithmetic_test(av, "-eq"));
+	else if (!ft_strcmp(av[2], "-ne"))
+		return (arithmetic_test(av, "-ne"));
+	else if (!ft_strcmp(av[2], "-ge"))
+		return (arithmetic_test(av, "-ge"));
+	else if (!ft_strcmp(av[2], "-lt"))
+		return (arithmetic_test(av, "-lt"));
+	else if (!ft_strcmp(av[2], "-le"))
+		return (arithmetic_test(av, "-le"));
+	else if (av[2][0] == '-')
+	{
+		ft_putstr_fd("42sh: unknown condition: ", 2);
 		ft_putendl_fd(av[1], 2);
 		return (2);
 	}
-	ft_putstr_fd("42sh: parse error: condition expected: ",2);
+	ft_putstr_fd("42sh: parse error: condition expected: ", 2);
 	ft_putendl_fd(av[2], 2);
 	return (1);
 }
 
-static int	suit_unary_test(char **av,struct stat *buf)
+static int	suit_unary_test(char **av, struct stat *buf)
 {
-	if (!ft_strcmp(av[1],"-u"))	//teste >> chmod u+s file ou chmod 4644 file 
+	if (!ft_strcmp(av[1], "-u"))	//teste >> chmod u+s file ou chmod 4644 file 
 		return ((buf->st_mode & S_ISUID) == 0);
-	else if (!ft_strcmp(av[1],"-s"))     //tested 
+	else if (!ft_strcmp(av[1], "-s"))     //tested 
 		return (!(buf->st_size > 0));
-	else if (!ft_strcmp(av[1],"-r")) // tested , chmod 377 to disable read  
-		return(access(av[2], R_OK) != 0);
-	else if (!ft_strcmp(av[1],"-w"))     //tested 
-		return((access(av[2], W_OK) != 0)); 
-	else if (!ft_strcmp(av[1],"-x"))     //tested 
-		return((access(av[2], X_OK) != 0)); 
-	else if (!ft_strcmp(av[1],"-z"))     // tested 
+	else if (!ft_strcmp(av[1], "-r")) // tested , chmod 377 to disable read  
+		return (access(av[2], R_OK) != 0);
+	else if (!ft_strcmp(av[1], "-w"))     //tested 
+		return ((access(av[2], W_OK) != 0)); 
+	else if (!ft_strcmp(av[1], "-x"))     //tested 
+		return ((access(av[2], X_OK) != 0)); 
+	else if (!ft_strcmp(av[1], "-z"))     // tested 
 		return ((ft_strlen(av[2]) != 0));   	
 	else if(av[1][0] == '-')
 	{	
@@ -143,21 +142,21 @@ int		test_builtin(int ac, char **av)
 	while ((j = 1) && av[++i] && !ft_strcmp(av[i],"!"))
 		ft_strdel(&av[i]);
 	not =  ((i - 1) && !(i % 2));
-	ac = ac - i +  not;
+	ac = ac - i;
 	while (i &&	av[i])
 		av[j++] = av[i++];
 	while(av[j])
 		av[j++] = NULL;
 	if(ac == 0)
-		return(1);
+		return (!not);
 	else if(ac == 1)
 		return (not & (int)av[1]);
 	else if(ac == 2 && not)
 			return (not ^ (int)av[2]) ;
 	else if(ac == 2)
-		return (unary_test(av));
+		return (not ^ unary_test(av));
 	else if(ac == 3)
-		return (binary_test(av));
+		return (not ^ binary_test(av));
 	ft_putendl_fd("test: too many arguments",2);	
 	return (1);
 }
