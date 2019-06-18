5a932420a6e7a422f6eaf367af59cb8d3c96f14e
ghariani <gmelek@student.42.fr>
2019-06-18 11:38:36 +0200

    fix [ in test_builtin

--- srcs/executor/test_builtin.c
+++ srcs/executor/test_builtin.c
@@ -6,7 +6,7 @@
 /*   By: ghamelek <ghamelek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 00:34:44 by ghamelek          #+#    #+#             */
-/*   Updated: 2019/06/17 20:43:24 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/06/18 11:30:19 by ghamelek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,15 +95,7 @@ static int	suit_unary_test(char **av, struct stat *buf)
 	else if (!ft_strcmp(av[1], "-z"))     // tested
 		return ((ft_strlen(av[2]) != 0));
 	else if(av[1][0] == '-')
-	{
-		ft_putstr_fd(av[0],2);
-		ft_putstr_fd(": unknown condition: ",2);
-		ft_putendl_fd(av[1], 2);
-		return (2);
-	}
-	ft_putstr_fd("42sh: parse error: condition expected: ",2);
-	ft_putendl_fd(av[1], 2);
-	return (1);
+	{ ft_putstr_fd(av[0],2); ft_putstr_fd(": unknown condition: ",2); ft_putendl_fd(av[1], 2); return (2); } ft_putstr_fd("42sh: parse error: condition expected: ",2); ft_putendl_fd(av[1], 2); return (1);
 }
 
 static int	unary_test(char **av)
@@ -134,26 +126,30 @@ static int	unary_test(char **av)
 	return (suit_unary_test(av,buf));
 }
 
-int		test_builtin(int ac, char **av)
+int 	builtin_tools(int *ac, char **av)
 {
-	int i ;
-	int j ;
-	int not ;
-
-	i = 0;
 	if (ft_strequ(av[0], "["))
 	{
-		if (ft_strequ(av[ac - 1], "]"))
-		{
-			ft_strdel(&av[ac - 1]);
-			ac--;
-		}
+		if (ft_strequ(av[*ac - 1], "]"))
+			ft_strdel(&av[--*ac]);
 		else
 		{
 			ft_putendl_fd("[: ']' expected", 2);
 			return (1);
 		}
 	}
+	return(0);
+}
+
+int		test_builtin(int ac, char **av)
+{
+	int i ;
+	int j ;
+	int not ;
+
+	if (builtin_tools(&ac, av))
+		return (1);
+	i = 0;
 	while ((j = 1) && av[++i] && !ft_strcmp(av[i],"!"))
 		ft_strdel(&av[i]);
 	not =  ((i - 1) && !(i % 2));
@@ -162,15 +158,13 @@ int		test_builtin(int ac, char **av)
 		av[j++] = av[i++];
 	while(av[j])
 		av[j++] = NULL;
-	if(ac == 0)
+	if (ac == 0)
 		return (!not);
-	else if(ac == 1)
-		return (not & (int)av[1]);
-	else if(ac == 2 && not)
-			return (not ^ (int)av[2]) ;
-	else if(ac == 2)
+	else if ((ac == 1) || (ac == 2 && not))
+		return (not & (int)av[ac]);
+	else if (ac == 2)
 		return (not ^ unary_test(av));
-	else if(ac == 3)
+	else if (ac == 3)
 		return (not ^ binary_test(av));
 	ft_putendl_fd("test: too many arguments",2);
 	return (1);
