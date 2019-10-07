68d27a1cb6a6d937a838e63e5404dc6ea221a2ee
Achraf <macadelic1030@gmail.com>
2019-10-07 17:13:53 +0200

    Ajout de gestion d'erreur fc -e -r -e -e -e -e -e -e

--- srcs/executor/fc_builtin_editor.c
+++ srcs/executor/fc_builtin_editor.c
@@ -6,7 +6,7 @@
 /*   By: aben-azz <aben-azz@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 19:37:17 by aben-azz          #+#    #+#             */
-/*   Updated: 2019/10/05 18:13:06 by aben-azz         ###   ########.fr       */
+/*   Updated: 2019/10/07 17:12:31 by aben-azz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,29 @@ int		fc_editor(int argc, char **argv, int param)
 	//char *string;
 
 	fcedit = get_key_value("FCEDIT", g_shell->env);
-	ft_printf("ac: %d editor: %s, fcedit: %s\n", argc, NULL, fcedit);
+	int i = 1;
+	while (i < argc && argv[i][0] == '-' && !~ft_indexof(argv[i], 'e'))
+		i++;
+	while (i < argc && argv[i + 1] && argv[i + 1][0] == '-' && argv[i + 1][1] == 'e')
+		i++;
+	ft_printf("%d/%d\n", i, argc);
+	if (argc - 1 > i && argv[i + 1][0] == '-')
+	{
+			ft_printf("erreur param apres -e\n");
+			return 1;
+
+	}
+	if (i == argc - 1)
+		ft_printf("0 argv\n");
+	else
+	{
+		i++;
+		ft_printf("ac: %d i: %d, arg: |%s| fcedit: %s\n", argc, i, argv[i], fcedit);
+		if (argc - 1 > i)
+			ft_printf("ya d args supp : {%s, %s}\n", argv[i + 1], argc - 1 - i == 2 ? argv[i + 2] : NULL);
+		else
+			ft_printf("ya pa darg supp\n");
+	}
 	//char *command[] = {"atom", "--wait", "~/.42sh_history", NULL};
 	// int pid;
 	// pid = fork();
